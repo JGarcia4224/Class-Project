cb7431b4c787544c0fd41b9e0cd5c7da5f19fb37
Jesse Garcia <Jesse.Garcia4224@gmail.com>
2018-05-15 21:35:59 -0700

    final commit

---
+++ crud-demo/routes/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const user = require("../controllers/user");
+
+// Get all users
+router.get("/", user.list);
+
+// Save user
+router.post("/user", user.create);
+
+// Edit or create a user
+router.get("/user/:id?", user.edit);
+
+//Delete user
+router.delete("/user/:id", user.delete);
+
+module.exports = router;
